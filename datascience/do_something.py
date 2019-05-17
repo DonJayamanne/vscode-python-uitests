88f3d67998ce8542faadbbbd8b71b8b315699797
Don Jayamanne <don.jayamanne@yahoo.com>
2019-05-17 15:24:55 -0700

    Misc

---
+++ datascience/do_something.py
@@ -0,0 +1,4 @@
+
+
+def print_guid():
+	print("080d2f6d-49df-42c5-97e6-2424edb15809")
---
+++ datascience/smoke.py
@@ -0,0 +1,9 @@
+#%%
+from IPython.display import Image
+Image(filename='image.png')
+
+#%%
+from do_something import print_guid
+print_guid()
+import sys
+print(sys.base_exec_prefix)
